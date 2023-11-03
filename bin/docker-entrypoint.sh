1025bbec7535e460841cceb622f49a918d54db9b
spryffee <akhmed-gasanov@outlook.com>
2023-11-03 18:58:09 +0000

    Prepare production Dockerfile

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-set -e
+#!/bin/bash -e
 
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /usr/src/app/tmp/pids/server.pid
+# If running the rails server then create or migrate existing database
+if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
+  ./bin/rails db:prepare
+fi
 
-echo "bundle install..."
-bundle check || bundle install --jobs 4
-
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+exec "${@}"
