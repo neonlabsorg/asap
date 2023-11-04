c9df334bb79b942447fabab7b5f3b521080d9101
spryffee <akhmed-gasanov@outlook.com>
2023-11-04 21:59:01 +0000

    Add dummy credentials to application config

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Asap
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
     config.exceptions_app = self.routes # TO USE CUSTOM 404, 500 pages
     # Configuration for the application, engines, and railties goes here.
     #
@@ -21,17 +21,17 @@ module Asap
 
 
     # General configuration
-    config.api_token                        = ENV.fetch("API_TOKEN")
-    config.db_password                      = ENV.fetch("DB_PASSWORD")
+    config.api_token                        = ENV.fetch("API_TOKEN") { Rails.application.credentials[:api_token] }
+    config.db_password                      = ENV.fetch("DB_PASSWORD") { Rails.application.credentials[:db_password] }
 
     # Active Directory configuration
-    config.ldap_host                        = ENV.fetch("LDAP_HOST")
+    config.ldap_host                        = ENV.fetch("LDAP_HOST") { "ldap.dummy.host" }
     config.ldap_port                        = ENV.fetch("LDAP_PORT") { 636 }
-    config.ldap_base                        = ENV.fetch("LDAP_BASE")
-    config.ldap_username                    = ENV.fetch("LDAP_USERNAME")
-    config.ldap_password                    = ENV.fetch("LDAP_PASSWORD")
+    config.ldap_base                        = ENV.fetch("LDAP_BASE") { "ldap.dummy.base" }
+    config.ldap_username                    = ENV.fetch("LDAP_USERNAME") { "ldap.dummy.username" }
+    config.ldap_password                    = ENV.fetch("LDAP_PASSWORD") { "ldap.dummy.password" }
     config.ldap_users_filter                = ENV.fetch("LDAP_USERS_FILTER") { "(&(objectCategory=Person)(!(UserAccountControl:1.2.840.113556.1.4.803:=2)))" } 
-    config.ldap_users_searchbase            = ENV.fetch("LDAP_USERS_SEARCHBASE")
+    config.ldap_users_searchbase            = ENV.fetch("LDAP_USERS_SEARCHBASE") {"ldap.dummy.searchbase"}
     config.ldap_attributes_mapping          = { 
       id:           :objectGUID, 
       displayname:  :displayName, 
