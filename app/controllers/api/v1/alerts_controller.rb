4b53318c2c8c66b1ce1a965b23de384234a1f145
spryffee <spryffee@gmail.com>
2024-04-01 12:41:21 +0300

    Refactor alerts api controller

--- app/controllers/api/v1/alerts_controller.rb
+++ app/controllers/api/v1/alerts_controller.rb
@@ -1,43 +1,16 @@
 class Api::V1::AlertsController < ApplicationController
   before_action :auth_from_token
-  skip_before_action :verify_authenticity_token # to avoid CORS issue
-  before_action :generate_uuid, only: %i[create]
-  before_action :set_alert, only: %i[create]
+  skip_before_action :verify_authenticity_token
+  before_action :generate_uuid, only: [:create]
+  before_action :set_alert, only: [:create]
 
   # POST /api/v1/alerts
   def create
-
     if @alert
-      if @alert.active && alert_attributes[:active] == 'false'
-        if @alert.update(last_closed_at: Time.now, last_closed_by: 'bot', active: false)
-          render json: { data: @alert }
-        else
-          render json: @alert.errors, status: :unprocessable_entity
-        end
-      end
-
-      if !@alert.active && alert_attributes[:active] == 'true'
-        if @alert.update(alert_attributes.merge(last_detected_at: Time.now, active: true, resurfaced: true))
-          render json: { data: @alert }
-        else
-          render json: @alert.errors, status: :unprocessable_entity
-        end
-      end
-
-      if @alert.update(alert_attributes.merge(last_detected_at: Time.now, active: true))
-        render json: { data: @alert }
-      else
-        render json: @alert.errors, status: :unprocessable_entity
-      end
+      handle_alert
     else
-      @alert = Alert.new(alert_attributes.merge(id: @uuid, last_detected_at: Time.now))
-      if @alert.save
-        render json: { data: @alert }, status: :created
-      else
-        render json: @alert.errors, status: :unprocessable_entity
-      end
+      create_alert
     end
-
   end
 
   # POST /api/v1/arrange_alerts
@@ -48,36 +21,77 @@ class Api::V1::AlertsController < ApplicationController
     #   source: "dummy-watcher",
     #   current_alerts: [ "id-1", "id-2", "id-n" ]
     # }
-
-    all_active_alerts = Alert.where(source: params[:source], active: true).ids if params[:source].present?
-    if all_active_alerts.any?
-      resolved_alerts = all_active_alerts - params[:current_alerts]
-      if resolved_alerts.any?
-        if Alert.where(id: resolved_alerts).update_all(
-          last_closed_at: Time.now, 
-          last_closed_by: 'bot', 
-          active: false
-          )
-          head :ok
-        end
-      end
-    end
-
+    close_resolved_alerts
   end
-
 
   private
 
   def generate_uuid
-    @uuid = Digest::UUID.uuid_v3(Digest::UUID::DNS_NAMESPACE, alert_attributes[:title] + alert_attributes[:asset])
+    @uuid = Digest::UUID.uuid_v3(Digest::UUID::DNS_NAMESPACE, alert_params[:title] + alert_params[:asset])
   end
 
   def set_alert
     @alert = Alert.find_by(id: @uuid)
   end
 
-  def alert_attributes
+  def alert_params
     params.permit(:title, :asset, :severity, :remediation, :active, :source, :issue,
                   :output, :nessus_plugin_id, :dc, cve_list: [])
+  end
+
+  def handle_alert
+    if @alert.active && alert_params[:active] == 'false'
+      close_alert
+    elsif !@alert.active && alert_params[:active] == 'true'
+      reopen_alert
+    else
+      update_alert
+    end
+  end
+
+  def close_alert
+    update_alert_attributes(last_closed_at: Time.now, last_closed_by: 'bot', active: false)
+  end
+
+  def reopen_alert
+    update_alert_attributes(alert_params.merge(last_detected_at: Time.now, active: true, resurfaced: true))
+  end
+
+  def update_alert
+    update_alert_attributes(alert_params.merge(last_detected_at: Time.now, active: true))
+  end
+
+  def update_alert_attributes(attributes)
+    if @alert.update(attributes)
+      render json: { data: @alert }
+    else
+      render json: @alert.errors, status: :unprocessable_entity
+    end
+  end
+
+  def create_alert
+    @alert = Alert.new(alert_params.merge(id: @uuid, last_detected_at: Time.now))
+    if @alert.save
+      render json: { data: @alert }, status: :created
+    else
+      render json: @alert.errors, status: :unprocessable_entity
+    end
+  end
+
+  def close_resolved_alerts
+    return unless params[:source].present?
+
+    all_active_alerts = Alert.where(source: params[:source], active: true).ids
+    resolved_alerts = all_active_alerts - params[:current_alerts]
+
+    return unless resolved_alerts.any?
+
+    if Alert.where(id: resolved_alerts).update_all(
+         last_closed_at: Time.now,
+         last_closed_by: 'bot',
+         active: false
+       )
+      head :ok
+    end
   end
 end
