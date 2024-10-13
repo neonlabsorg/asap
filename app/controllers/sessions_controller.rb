16c6b7485f0b07ca3c470b07cbcb6c0787d6337c
spryffee <spryffee@gmail.com>
2024-10-14 00:12:32 +0300

    [test] identify user by email in sso

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -69,8 +69,8 @@ class SessionsController < ApplicationController
 
   def auth_sso
     auth = request.env['omniauth.auth']
-    id =  auth['uid'] # uid corresponds to sub claim
-    user = User.find_by(id: id)
+    email =  auth['extra']['raw_info']['email'] # uid corresponds to sub claim
+    user = User.find_by(email: email)
   end
 
   def auth_noauth
