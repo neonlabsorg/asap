78f9db76bb05becd853322692e99ee2052277682
spryffee <spryffee@gmail.com>
2025-05-27 13:11:49 +0300

    Disable force SSL for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,39 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.find_or_create_by(
-  email: "admin@example.com", 
-  displayname: "Administrator", 
-  username: "admin") if User.none?
+# User.find_or_create_by(
+#   email: "admin@example.com", 
+#   displayname: "Administrator", 
+#   username: "admin") if User.none?
 
+
+# Create default users if they don't exist
+default_users = [
+  {
+    email: "ahmed.gasanov@neonlabs.org",
+    displayname: "Ahmed Gasanov",
+    username: "ahmed.gasanov@neonlabs.org"
+  },
+  {
+    email: "sergei.bondar@neonlabs.org",
+    displayname: "Sergei Bondar",
+    username: "sergei.bondar@neonlabs.org"
+  },
+  {
+    email: "vitalii.bicov@neonlabs.org",
+    displayname: "Vitalii Bicov",
+    username: "vitalii.bicov@neonlabs.org"
+  }
+]
+
+default_users.each do |user_attrs|
+  User.find_or_create_by!(username: user_attrs[:username]) do |user|
+    user.email = user_attrs[:email]
+    user.displayname = user_attrs[:displayname]
+  end
+end
+
+# Create sample alert if no alerts exist
 Alert.find_or_create_by(
   title: "Sample Alert 1",
   asset: "Sample Asset 1",
